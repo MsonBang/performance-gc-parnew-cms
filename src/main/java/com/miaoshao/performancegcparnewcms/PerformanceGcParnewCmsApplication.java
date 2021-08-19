d5951aa95a830538f897d42f073680126fc7144d
miaoxuebing <352835148@qq.com>
2021-08-19 15:05:21 +0800

    垃圾收集器和底层算法笔记第一版

---
+++ src/main/java/com/miaoshao/performancegcparnewcms/PerformanceGcParnewCmsApplication.java
@@ -0,0 +1,13 @@
+package com.miaoshao.performancegcparnewcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceGcParnewCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceGcParnewCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miaoshao/performancegcparnewcms/PerformanceGcParnewCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaoshao.performancegcparnewcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceGcParnewCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
